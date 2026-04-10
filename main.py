c04306ddc0b4496b39b26c1b219cfd4a6d53c9fa
dcmorris05 <dcmorris2205@gmail.com>
2026-04-09 23:25:28 -0700

    def funcs
    floor, ciel, injuctive par

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ def find_non_injective_pair(mapping: dict) -> tuple | None:
     """Return (x1, x2) where f(x1)==f(x2) and x1!=x2, or None if injective."""
     # === TODO ===
     # Your code here
-    pass
+    mappingDupe = mapping.copy()
+    for comparee in mapping:
+        #iterates len(mapping) - 1 times
+        #comparee = key
+        mappingDupe.pop(comparee)
+        for compared in mappingDupe:
+            #iterates len(mappingDupe) - 1 times
+            #mapping dupe is always 1 shorter each iteration of mapping
+            
+            
     # === END TODO ===
 
 
@@ -16,12 +25,11 @@ def find_non_surjective_element(mapping: dict, target: set):
     pass
     # === END TODO ===
 
-
 def my_floor(x: float) -> int:
     """Return floor(x) without using math.floor."""
     # === TODO ===
     # Your code here
-    pass
+    return int(x) if x >= 0 else -my_ceil(-x)
     # === END TODO ===
 
 
@@ -29,5 +37,10 @@ def my_ceil(x: float) -> int:
     """Return ceil(x) without using math.ceil."""
     # === TODO ===
     # Your code here
-    pass
+    #return int(x) if float(int(x)) == x
+    #return int(x) if x % 1 == 0.0 else 
+    #if float(int(x)) == x: return int(x)
+    #return int(x) + 1 if x >= 0 else int(x)
+    #return int(x) if float(int(x)) == x or x >= 0 else int(x)
+    return int(x) if float(int(x)) == x or x <= 0 else int(x) + 1
     # === END TODO ===
